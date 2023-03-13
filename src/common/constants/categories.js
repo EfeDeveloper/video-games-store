6c3bb057debe1bf34d686f9aeadac7afdf4b56fa
EfeDeveloper <edwinvilla_adsi@outlook.com>
2023-03-12 20:19:44 -0500

    ✨ feat/Sidebar and content are added:
    1. Add the sidebar and content.
    2. Change the primary color in tailwind.config.
    3. Added custom scrolling arra
    4. Removed react.svg icon
    5. Added category and platform constants.

---
+++ src/common/constants/categories.js
@@ -0,0 +1,34 @@
+export const categories = [
+  {
+    id: 1,
+    label: 'Indy',
+  },
+  {
+    id: 2,
+    label: 'Adventure',
+  },
+  {
+    id: 3,
+    label: 'MMO',
+  },
+  {
+    id: 4,
+    label: 'Casual game',
+  },
+  {
+    id: 5,
+    label: 'Strategy',
+  },
+  {
+    id: 6,
+    label: 'Simulator',
+  },
+  {
+    id: 7,
+    label: 'Sports Game',
+  },
+  {
+    id: 8,
+    label: 'Action Game',
+  },
+];
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Content = () => {
+  return <div className="content grid h-screen place-items-center">Content</div>;
+};
+
+export default Content;
