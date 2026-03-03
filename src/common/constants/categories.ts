f3a36442913305c380b842f94935502097cddb7e
Edwin F. Villa <edwin.villa.ext@terpel.com>
2026-03-03 18:30:25 -0500

    👷[chore]Clean up unused code and improve project structure
    - Removed redundant code blocks to enhance readability.
    - Organized files into appropriate directories for better maintainability.
    - Updated comments for clarity and consistency across the codebase.

---
+++ src/common/constants/categories.ts
@@ -0,0 +1,44 @@
+import { Category } from '@/types';
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    label: 'Indie',
+    slug: 'indie',
+  },
+  {
+    id: 2,
+    label: 'Adventure',
+    slug: 'adventure',
+  },
+  {
+    id: 3,
+    label: 'MMO',
+    slug: 'massively-multiplayer',
+  },
+  {
+    id: 4,
+    label: 'Casual',
+    slug: 'casual',
+  },
+  {
+    id: 5,
+    label: 'Strategy',
+    slug: 'strategy',
+  },
+  {
+    id: 6,
+    label: 'Simulator',
+    slug: 'simulation',
+  },
+  {
+    id: 7,
+    label: 'Sports',
+    slug: 'sports',
+  },
+  {
+    id: 8,
+    label: 'Action',
+    slug: 'action',
+  },
+];
